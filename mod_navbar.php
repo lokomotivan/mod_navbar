0b9730f19efba72d9fdb164125e2093bbb5af7d0
Ivan Milincic <lokomotivan@gmail.com>
2017-09-23 20:14:28 +0200

    uplaod

---
+++ mod_navbar.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_navbar
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+// Include the syndicate functions only once
+require_once __DIR__ . '/helper.php';
+
+// add custom js
+$document = JFactory::getDocument();
+$document->addScript(JURI::base(true) . '/modules/mod_navbar/js/bootstrap-hover-dropdown.min.js');
+
+$list      = ModNavbarHelper::getList($params);
+$base      = ModNavbarHelper::getBase($params);
+$active    = ModNavbarHelper::getActive($params);
+$active_id = $active->id;
+$path      = $base->tree;
+$showAll   = $params->get('showAllChildren');
+$class_sfx = htmlspecialchars($params->get('class_sfx'));
+
+if (count($list))
+{
+	require JModuleHelper::getLayoutPath('mod_navbar', $params->get('layout', 'default'));
+}
